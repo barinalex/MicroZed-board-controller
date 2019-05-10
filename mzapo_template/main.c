490f2a59defe7c3341f7e7cca42dd8181679237c
barinalex <barinale@fel.cvut.cz>
2019-05-10 12:06:33 +0200

    structures added

--- mzapo_template/main.c
+++ mzapo_template/main.c
@@ -31,6 +31,8 @@ typedef struct{
 } rect_;
 
 typedef struct{
+	int buttons_number;
+	int menu_pos;
 	char *button0;
 	char *button1;
 	char *button2;
@@ -40,9 +42,18 @@ typedef struct{
 	char *comment;
 } menu_;
 
-menu_ menu;
+typedef struct{
+	uint32_t rgb_value;
+	uint8_t r_knob;
+	uint8_t g_knob;
+	uint8_t b_knob;
+	uint8_t r_button;
+	uint8_t g_button;
+	uint8_t b_button;	
+} knobs_;
+
 unsigned char *mem_base;
-uint32_t *knobs;
+uint32_t *knobs_base;
 unsigned char *led1_mem_base;
 unsigned char *led2_mem_base;
 uint32_t led1_rgb_value;
@@ -53,19 +64,18 @@ int get_knob_value();
 void get_rgb_values(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b);
 void get_buttons_value(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b);
 void to_led(unsigned char *led_mem_base, uint32_t rgb_knobs_value);
-void rectangle_to_lcd(uint32_t rgb_knobs_value, rect_ rect);
+void rectangle_to_lcd(knobs_ knobs, rect_ rect);
 void clear_screen();
 void static_menu();
 int initialize_adresses();
-void menu_template(int menu_pos, int buttons_number);
-void rect_template(int menu_pos, uint32_t rgb_knobs_value);
+void menu_template(menu_ menu);
+rect_ set_rect(int menu_pos);
+void get_knobs_data(knobs_ *knobs);
 	
-void rectangle_to_lcd(uint32_t rgb_knobs_value, rect_ rect){
-	uint8_t rk, gk, bk;
+void rectangle_to_lcd(knobs_ knobs, rect_ rect){
 	uint16_t color = 0;
-	printf("%x\n", *knobs);
-	get_rgb_values(rgb_knobs_value, &rk, &gk, &bk);
-	color = ((rk >> 3) << 11) | ((gk >> 2) << 5) | (bk >> 3);
+	printf("%x\n", *knobs_base);
+	color = ((knobs.r_knob >> 3) << 11) | ((knobs.g_knob >> 2) << 5) | (knobs.b_knob >> 3);
 	for (int r = 0; r < 320 ; r++) {
 		for (int c = 0; c < 480 ; c++) {
 			if(r >= rect.top && r < rect.bottom && c >= rect.left && c < rect.right){
@@ -88,54 +98,14 @@ void to_led(unsigned char *led_mem_base, uint32_t rgb_knobs_value){
 	*(led_mem_base+2) = rk;
 }
 
-void draw_main_menu(int menu_pos){
-	strToFrame("Static", 50, 50, WHITE, BLACK);
-	strToFrame("Continuous", 150, 50, WHITE, BLACK);
-	strToFrame("Flashing", 250, 50, WHITE, BLACK);
-	switch(menu_pos){
-		case 0:
-			strToFrame("Static", 50, 50, BLUE, BLACK);
-			break;
-		case 1:
-			strToFrame("Continuous", 150, 50, BLUE, BLACK);
-			break;
-		case 2:
-			strToFrame("Flashing", 250, 50, BLUE, BLACK);
-			break;
-	}
-	frameToLCD();
-}
-
-void draw_static_menu(int menu_pos){
-	strToFrame("Led 1", 50, 50, WHITE, BLACK);
-	strToFrame("Led 2", 120, 50, WHITE, BLACK);
-	strToFrame("Both", 190, 50, WHITE, BLACK);
-	strToFrame("Copy", 260, 50, WHITE, BLACK);
-	switch(menu_pos){
-		case 0:
-			strToFrame("Led 1", 50, 50, BLUE, BLACK);
-			break;
-		case 1:
-			strToFrame("Led 2", 120, 50, BLUE, BLACK);
-			break;
-		case 2:
-			strToFrame("Both", 190, 50, BLUE, BLACK);
-			break;
-		case 3:
-			strToFrame("Copy", 260, 50, BLUE, BLACK);
-			break;
-	}
-	frameToLCD();
-}
-
-void menu_template(int menu_pos, int buttons_number){
-	if(buttons_number > 0) strToFrame(menu.button0, 10, 50, WHITE, BLACK);
-	if(buttons_number > 1) strToFrame(menu.button1, 60, 50, WHITE, BLACK);
-	if(buttons_number > 2) strToFrame(menu.button2, 110, 50, WHITE, BLACK);
-	if(buttons_number > 3) strToFrame(menu.button3, 160, 50, WHITE, BLACK);
-	if(buttons_number > 4) strToFrame(menu.button4, 210, 50, WHITE, BLACK);
-	if(buttons_number > 5) strToFrame(menu.button5, 260, 50, WHITE, BLACK);
-	switch(menu_pos){
+void menu_template(menu_ menu){
+	if(menu.buttons_number > 0) strToFrame(menu.button0, 10, 50, WHITE, BLACK);
+	if(menu.buttons_number > 1) strToFrame(menu.button1, 60, 50, WHITE, BLACK);
+	if(menu.buttons_number > 2) strToFrame(menu.button2, 110, 50, WHITE, BLACK);
+	if(menu.buttons_number > 3) strToFrame(menu.button3, 160, 50, WHITE, BLACK);
+	if(menu.buttons_number > 4) strToFrame(menu.button4, 210, 50, WHITE, BLACK);
+	if(menu.buttons_number > 5) strToFrame(menu.button5, 260, 50, WHITE, BLACK);
+	switch(menu.menu_pos){
 		case 0:
 			strToFrame(menu.button0, 10, 50, BLUE, BLACK);
 			break;
@@ -158,7 +128,7 @@ void menu_template(int menu_pos, int buttons_number){
 	frameToLCD();
 }
 
-void rect_template(int menu_pos, uint32_t rgb_knobs_value){
+rect_ set_rect(int menu_pos){
 	rect_ rect;
 	rect.left = 40;
 	rect.right = 400;
@@ -188,7 +158,7 @@ void rect_template(int menu_pos, uint32_t rgb_knobs_value){
 			rect.bottom = 300;
 			break;
 	}
-	rectangle_to_lcd(rgb_knobs_value, rect);
+	return rect;
 }
 
 void get_rgb_values(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b){
@@ -244,77 +214,79 @@ void static_menu_led_control(int menu_pos, int rgb_knobs_value, int *last_change
 
 void static_menu(){
 	clear_screen();
-	uint32_t rgb_knobs_value;
-	uint16_t blue_knob_value;
-	uint16_t prev_blue_knob_value = get_knob_value()  & 0xFF;;
-	uint8_t blue_button, green_button, red_button;
-	int menu_pos = 0, buttons_number = 4;
 	bool choosing_color = false;
 	int last_changed_led = 0;
 	
+	knobs_ knobs;
+	get_knobs_data(&knobs);
+	uint8_t prev_blue_knob_value = knobs.b_knob;
+	
+	menu_ menu;
+	menu.buttons_number = 4;
+	menu.menu_pos = 0;
 	menu.button0 = "Led 1";
 	menu.button1 = "Led";
 	menu.button2 = "Both";
 	menu.button3 = "Copy";
 	
 	while(true){
-		rgb_knobs_value = get_knob_value();
-		blue_knob_value =  rgb_knobs_value & 0xFF;
-		get_buttons_value(rgb_knobs_value, &red_button, &green_button, &blue_button);
-		blue_button = (rgb_knobs_value>>24) & 1;
-		red_button = (rgb_knobs_value>>26) & 1;
-		if(red_button) {
+		if(knobs.r_button) {
 			usleep(DELAY);
 			clear_screen();
 			break;
 		}
-		if(blue_button) {
+		if(knobs.b_button) {
 			usleep(DELAY);
 			clear_screen();
 			choosing_color = !choosing_color;
 		}
 		if(!choosing_color){
-			menu_pos = change_menu_pos(buttons_number, blue_knob_value, prev_blue_knob_value, menu_pos);
-		}else{
-			rect_template(menu_pos, rgb_knobs_value);
-			static_menu_led_control(menu_pos,rgb_knobs_value, &last_changed_led);
-			choosing_color = (menu_pos == 3) ? false: choosing_color;
+			menu.menu_pos = change_menu_pos(menu.buttons_number, knobs.b_knob, prev_blue_knob_value, menu.menu_pos);
+		}else{	
+			rectangle_to_lcd(knobs, set_rect(menu.menu_pos));
+			static_menu_led_control(menu.menu_pos, knobs.rgb_value, &last_changed_led);
+			if(menu.menu_pos == 3) choosing_color = false;
 		}
-		prev_blue_knob_value = blue_knob_value;
-		menu_template(menu_pos, buttons_number);
+		prev_blue_knob_value = knobs.b_knob;
+		menu_template(menu);
 	}
 }
 
-void set_main_menu_buttons(){
-	menu.button0 = "Static";
-	menu.button1 = "Continuous";
-	menu.button2 = "Flashing";
+void get_knobs_data(knobs_ *knobs){
+	knobs->rgb_value = get_knob_value();
+	knobs->b_knob =  knobs->rgb_value      & 0xFF; // blue knob position
+	knobs->g_knob = (knobs->rgb_value>>8)  & 0xFF; // green knob position
+	knobs->r_knob = (knobs->rgb_value>>16) & 0xFF; // red knob position
+	knobs->b_button = (knobs->rgb_value>>24) & 1; // blue button
+	knobs->g_button = (knobs->rgb_value>>25) & 1; // green button
+	knobs->r_button = (knobs->rgb_value>>26) & 1; // red buttom
 }
 
 int main(int argc, char *argv[])
 {
 	if(initialize_adresses() != 0) exit(1);
-	uint32_t rgb_knobs_value;
-	uint16_t blue_knob_value;
-	uint16_t prev_blue_knob_value = get_knob_value() & 0xFF;
-	uint8_t blue_button;
-	int menu_pos = 0, buttons_number = 4;
-	set_main_menu_buttons();
+	knobs_ knobs;
+	menu_ menu;
+	menu.buttons_number = 3;
+	menu.menu_pos = 0;
+	menu.button0 = "Static";
+	menu.button1 = "Continuous";
+	menu.button2 = "Flashing";
+	
+	get_knobs_data(&knobs);
+	uint8_t prev_b_knob = knobs.b_knob;
 
 		
 	while(true){
-		rgb_knobs_value = get_knob_value();
-		blue_knob_value =  rgb_knobs_value & 0xFF;
-		blue_button = (rgb_knobs_value>>24) & 1;
-		menu_pos = change_menu_pos(buttons_number, blue_knob_value, prev_blue_knob_value, menu_pos);
-		prev_blue_knob_value = blue_knob_value;
-		menu_template(menu_pos, buttons_number);		
+		get_knobs_data(&knobs);
+		menu.menu_pos = change_menu_pos(menu.buttons_number, knobs.b_knob, prev_b_knob, menu.menu_pos);
+		prev_b_knob = knobs.b_knob;
+		menu_template(menu);		
 		//draw_main_menu(menu_pos);
-		if(menu_pos == 0 && blue_button){
+		if(menu.menu_pos == 0 && knobs.b_button){
 			usleep(DELAY);
 			static_menu();
-			menu_pos = 0;
-			set_main_menu_buttons();
+			menu.menu_pos = 0;
 		}
 	}
 	return 0;
@@ -335,7 +307,7 @@ int initialize_adresses(){
 	parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
 	if (parlcd_mem_base == NULL) return 1;
 	parlcd_hx8357_init(parlcd_mem_base);
-	knobs = map_phys_address(SPILED_REG_BASE_PHYS + SPILED_REG_KNOBS_8BIT_o, 4, false);
+	knobs_base = map_phys_address(SPILED_REG_BASE_PHYS + SPILED_REG_KNOBS_8BIT_o, 4, false);
 	led1_mem_base = mem_base + SPILED_REG_LED_RGB1_o;
 	led2_mem_base = mem_base + SPILED_REG_LED_RGB2_o;
 	return 0;
