26c29f4ae94f107799a6c5fbc8b2482bf0ca7661
barinalex <barinale@fel.cvut.cz>
2019-05-10 09:41:07 +0200

    menu template

--- mzapo_template/main.c
+++ mzapo_template/main.c
@@ -1,14 +1,5 @@
 /*******************************************************************
   Simple program to check LCD functionality on MicroZed
-  based MZ_APO board designed by Petr Porazil at PiKRON
-
-  mzapo_lcdtest.c       - main and only file
-
-  (C) Copyright 2004 - 2017 by Pavel Pisa
-      e-mail:   pisa@cmp.felk.cvut.cz
-      homepage: http://cmp.felk.cvut.cz/~pisa
-      work:     http://www.pikron.com/
-      license:  any combination of GPL, LGPL, MPL or BSD licenses
 
  *******************************************************************/
 
@@ -17,6 +8,7 @@
 #define BLUE 0x001F
 #define WHITE 0xFFFF
 #define GREEN 0x07E0
+#define DELAY 100000
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -31,6 +23,24 @@
 #include "mzapo_regs.h"
 #include "lcdframe.h"
 
+typedef struct{
+	int top;
+	int bottom;
+	int left;
+	int right;
+} rect_;
+
+typedef struct{
+	char *button0;
+	char *button1;
+	char *button2;
+	char *button3;
+	char *button4;
+	char *button5;
+	char *comment;
+} menu_;
+
+menu_ menu;
 unsigned char *mem_base;
 uint32_t *knobs;
 unsigned char *led1_mem_base;
@@ -40,24 +50,25 @@ uint32_t led2_rgb_value;
 uint16_t frame[FRAME_H][FRAME_W];
 	
 int get_knob_value();
-void get_rgb_values(uint32_t rgb_knobs_value, uint16_t *r, uint16_t *g, uint16_t *b);
+void get_rgb_values(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b);
 void get_buttons_value(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b);
 void to_led(unsigned char *led_mem_base, uint32_t rgb_knobs_value);
+void rectangle_to_lcd(uint32_t rgb_knobs_value, rect_ rect);
 void clear_screen();
+void static_menu();
+int initialize_adresses();
+void menu_template(int menu_pos, int buttons_number);
+void rect_template(int menu_pos, uint32_t rgb_knobs_value);
 	
-void rectangle_to_lcd(uint32_t rgb_knobs_value, int row, int column, int width, int height){
-	uint16_t rk, gk, bk;
+void rectangle_to_lcd(uint32_t rgb_knobs_value, rect_ rect){
+	uint8_t rk, gk, bk;
 	uint16_t color = 0;
 	printf("%x\n", *knobs);
-	parlcd_write_cmd(parlcd_mem_base, 0x2c);
-	
 	get_rgb_values(rgb_knobs_value, &rk, &gk, &bk);
-
 	color = ((rk >> 3) << 11) | ((gk >> 2) << 5) | (bk >> 3);
-			
 	for (int r = 0; r < 320 ; r++) {
 		for (int c = 0; c < 480 ; c++) {
-			if(r >= row && r < height + row && c >= column && c < width + column){
+			if(r >= rect.top && r < rect.bottom && c >= rect.left && c < rect.right){
 				frame[r][c] = color;
 			}
 		}
@@ -70,25 +81,11 @@ int get_knob_value(){
 }
 
 void to_led(unsigned char *led_mem_base, uint32_t rgb_knobs_value){
-	uint16_t rk, gk, bk; 
-	uint8_t rb, gb, bb;
+	uint8_t rk, gk, bk; 
 	get_rgb_values(rgb_knobs_value, &rk, &gk, &bk);
-	get_buttons_value(rgb_knobs_value, &rb, &gb, &bb);
-	
 	*led_mem_base = bk;
 	*(led_mem_base+1) = gk;
 	*(led_mem_base+2) = rk;
-	/*
-	if(bb == 1){
-		*led_mem_base = bb;
-	}
-	if(gb == 1){
-		*(led_mem_base+1) = gb;
-	}
-	if(rb == 1){
-		*(led_mem_base+2) = rb;
-	}
-	*/
 }
 
 void draw_main_menu(int menu_pos){
@@ -131,16 +128,79 @@ void draw_static_menu(int menu_pos){
 	frameToLCD();
 }
 
-void get_rgb_values(uint32_t rgb_knobs_value, uint16_t *r, uint16_t *g, uint16_t *b){
+void menu_template(int menu_pos, int buttons_number){
+	if(buttons_number > 0) strToFrame(menu.button0, 10, 50, WHITE, BLACK);
+	if(buttons_number > 1) strToFrame(menu.button1, 60, 50, WHITE, BLACK);
+	if(buttons_number > 2) strToFrame(menu.button2, 110, 50, WHITE, BLACK);
+	if(buttons_number > 3) strToFrame(menu.button3, 160, 50, WHITE, BLACK);
+	if(buttons_number > 4) strToFrame(menu.button4, 210, 50, WHITE, BLACK);
+	if(buttons_number > 5) strToFrame(menu.button5, 260, 50, WHITE, BLACK);
+	switch(menu_pos){
+		case 0:
+			strToFrame(menu.button0, 10, 50, BLUE, BLACK);
+			break;
+		case 1:
+			strToFrame(menu.button1, 60, 50, BLUE, BLACK);
+			break;
+		case 2:
+			strToFrame(menu.button2, 110, 50, BLUE, BLACK);
+			break;
+		case 3:
+			strToFrame(menu.button3, 160, 50, BLUE, BLACK);
+			break;
+		case 4:
+			strToFrame(menu.button4, 210, 50, BLUE, BLACK);
+			break;
+		case 5:
+			strToFrame(menu.button5, 260, 50, BLUE, BLACK);
+			break;
+	}
+	frameToLCD();
+}
+
+void rect_template(int menu_pos, uint32_t rgb_knobs_value){
+	rect_ rect;
+	rect.left = 40;
+	rect.right = 400;
+	switch(menu_pos){
+		case 0:
+			rect.top = 35;
+			rect.bottom = 50;
+			break;
+		case 1:
+			rect.top = 85;
+			rect.bottom = 100;
+			break;
+		case 2:
+			rect.top = 135;
+			rect.bottom = 150;
+			break;
+		case 3:
+			rect.top = 185;
+			rect.bottom = 200;
+			break;
+		case 4:
+			rect.top = 235;
+			rect.bottom = 250;
+			break;
+		case 5:
+			rect.top = 285;
+			rect.bottom = 300;
+			break;
+	}
+	rectangle_to_lcd(rgb_knobs_value, rect);
+}
+
+void get_rgb_values(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b){
 	*b =  rgb_knobs_value      & 0xFF; // blue knob position
 	*g = (rgb_knobs_value>>8)  & 0xFF; // green knob position
 	*r = (rgb_knobs_value>>16) & 0xFF; // red knob position
 }
 
 void get_buttons_value(uint32_t rgb_knobs_value, uint8_t *r, uint8_t *g, uint8_t *b){
-	*b = (rgb_knobs_value>>24) & 1;    // blue button
-	*g = (rgb_knobs_value>>25) & 1;    // green button
-	*r = (rgb_knobs_value>>26) & 1;    // red buttom
+	*b = (rgb_knobs_value>>24) & 1; // blue button
+	*g = (rgb_knobs_value>>25) & 1; // green button
+	*r = (rgb_knobs_value>>26) & 1; // red buttom
 }
 
 int change_menu_pos(int buttons_number, uint8_t button_value, uint8_t prev_button_value, int menu_pos){
@@ -155,68 +215,109 @@ int change_menu_pos(int buttons_number, uint8_t button_value, uint8_t prev_butto
 		return menu_pos;
 }
 
+void static_menu_led_control(int menu_pos, int rgb_knobs_value, int *last_changed_led){
+	switch(menu_pos){
+		case 0:
+			to_led(led1_mem_base, rgb_knobs_value);
+			led1_rgb_value = rgb_knobs_value;
+			*last_changed_led = 0;
+			break;
+		case 1:
+			to_led(led2_mem_base, rgb_knobs_value);
+			led2_rgb_value = rgb_knobs_value; 
+			*last_changed_led = 1;
+			break;
+		case 2:
+			to_led(led1_mem_base, rgb_knobs_value);
+			to_led(led2_mem_base, rgb_knobs_value);
+			break;
+		case 3:
+			if(*last_changed_led == 0){
+				to_led(led2_mem_base, led1_rgb_value);
+			}
+			else{
+				to_led(led1_mem_base, led2_rgb_value);
+			}
+			break;
+	}
+}
+
 void static_menu(){
 	clear_screen();
 	uint32_t rgb_knobs_value;
 	uint16_t blue_knob_value;
 	uint16_t prev_blue_knob_value = get_knob_value()  & 0xFF;;
 	uint8_t blue_button, green_button, red_button;
-	int menu_pos = 0;
+	int menu_pos = 0, buttons_number = 4;
 	bool choosing_color = false;
-	
 	int last_changed_led = 0;
+	
+	menu.button0 = "Led 1";
+	menu.button1 = "Led";
+	menu.button2 = "Both";
+	menu.button3 = "Copy";
 	
 	while(true){
 		rgb_knobs_value = get_knob_value();
 		blue_knob_value =  rgb_knobs_value & 0xFF;
 		get_buttons_value(rgb_knobs_value, &red_button, &green_button, &blue_button);
+		blue_button = (rgb_knobs_value>>24) & 1;
+		red_button = (rgb_knobs_value>>26) & 1;
 		if(red_button) {
+			usleep(DELAY);
 			clear_screen();
 			break;
 		}
 		if(blue_button) {
-			usleep(100000);
+			usleep(DELAY);
 			clear_screen();
 			choosing_color = !choosing_color;
 		}
-		
 		if(!choosing_color){
-			menu_pos = change_menu_pos(4, blue_knob_value, prev_blue_knob_value, menu_pos);
-			printf("menu_pos: %d\n",menu_pos);
+			menu_pos = change_menu_pos(buttons_number, blue_knob_value, prev_blue_knob_value, menu_pos);
+		}else{
+			rect_template(menu_pos, rgb_knobs_value);
+			static_menu_led_control(menu_pos,rgb_knobs_value, &last_changed_led);
+			choosing_color = (menu_pos == 3) ? false: choosing_color;
 		}
 		prev_blue_knob_value = blue_knob_value;
-		draw_static_menu(menu_pos);
-		if(choosing_color){
-			switch(menu_pos){
-				case 0:
-					rectangle_to_lcd(rgb_knobs_value, 80, 40, 400, 20);
-					to_led(led1_mem_base, rgb_knobs_value);
-					led1_rgb_value = rgb_knobs_value;
-					last_changed_led = 0;
-					break;
-				case 1:
-					rectangle_to_lcd(rgb_knobs_value, 150, 40, 400, 20);
-					to_led(led2_mem_base, rgb_knobs_value);
-					led2_rgb_value = rgb_knobs_value; 
-					last_changed_led = 1;
-					break;
-				case 2:
-					rectangle_to_lcd(rgb_knobs_value, 230, 40, 400, 20);
-					to_led(led1_mem_base, rgb_knobs_value);
-					to_led(led2_mem_base, rgb_knobs_value);
-					break;
-				case 3:
-					if(last_changed_led == 0){
-						to_led(led2_mem_base, led1_rgb_value);
-					}
-					else{
-						to_led(led1_mem_base, led2_rgb_value);
-					}
-					choosing_color = false;
-					break;
-			}
+		menu_template(menu_pos, buttons_number);
+	}
+}
+
+void set_main_menu_buttons(){
+	menu.button0 = "Static";
+	menu.button1 = "Continuous";
+	menu.button2 = "Flashing";
+}
+
+int main(int argc, char *argv[])
+{
+	if(initialize_adresses() != 0) exit(1);
+	uint32_t rgb_knobs_value;
+	uint16_t blue_knob_value;
+	uint16_t prev_blue_knob_value = get_knob_value() & 0xFF;
+	uint8_t blue_button;
+	int menu_pos = 0, buttons_number = 4;
+	set_main_menu_buttons();
+
+		
+	while(true){
+		rgb_knobs_value = get_knob_value();
+		blue_knob_value =  rgb_knobs_value & 0xFF;
+		blue_button = (rgb_knobs_value>>24) & 1;
+		menu_pos = change_menu_pos(buttons_number, blue_knob_value, prev_blue_knob_value, menu_pos);
+		prev_blue_knob_value = blue_knob_value;
+		menu_template(menu_pos, buttons_number);		
+		//draw_main_menu(menu_pos);
+		if(menu_pos == 0 && blue_button){
+			usleep(DELAY);
+			static_menu();
+			menu_pos = 0;
+			set_main_menu_buttons();
 		}
 	}
+	return 0;
 }
 
 void clear_screen(){
@@ -228,45 +329,15 @@ void clear_screen(){
 	frameToLCD();
 }
 
-int main(int argc, char *argv[])
-{
+int initialize_adresses(){
 	mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
-	if (mem_base == NULL) exit(1);
-
+	if (mem_base == NULL) return 1;
 	parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
-	if (parlcd_mem_base == NULL)  exit(1);
+	if (parlcd_mem_base == NULL) return 1;
 	parlcd_hx8357_init(parlcd_mem_base);
-		
 	knobs = map_phys_address(SPILED_REG_BASE_PHYS + SPILED_REG_KNOBS_8BIT_o, 4, false);
-	
 	led1_mem_base = mem_base + SPILED_REG_LED_RGB1_o;
 	led2_mem_base = mem_base + SPILED_REG_LED_RGB2_o;
-	
-	uint32_t rgb_knobs_value;
-	uint16_t blue_knob_value;
-	uint16_t prev_blue_knob_value;
-	uint8_t blue_button;
-	prev_blue_knob_value =  get_knob_value() & 0xFF;
-	int menu_pos = 0;
-		
-	while(true){
-		rgb_knobs_value = get_knob_value();
-		blue_knob_value =  rgb_knobs_value & 0xFF;
-		blue_button = (rgb_knobs_value>>24) & 1;
-		menu_pos = change_menu_pos(3, blue_knob_value, prev_blue_knob_value, menu_pos);
-		prev_blue_knob_value = blue_knob_value;
-		printf("menu_pos: %d\n",menu_pos);
-		
-		draw_main_menu(menu_pos);
-		if(menu_pos == 0 && blue_button){
-			usleep(100000);
-			static_menu();
-			menu_pos = 0;
-		}
-		//rectangle_to_lcd(rgb_knobs_value, 80, 100, 300, 3 0);
-		//to_led(led1_mem_base, rgb_knobs_value);
-		//to_led(led2_mem_base, rgb_knobs_value);
-	}
 	return 0;
 }
 
