27808e0544ace2bd359b7a306c4bf9d85a3d17b1
barinale <barinale@k09-207.felk.cvut.cz>
2019-05-17 17:18:07 +0200

    shift absence

--- mzapo_template/flash_menu.c
+++ mzapo_template/flash_menu.c
@@ -58,19 +58,29 @@ void change_flash_time(menu_ *menu){
 	}
 }
 
-void choose_shift_flash(menu_ *menu){	
-	led1.change = false;
-	led2.change = true;
-	choose_time(&(led1.flash.shift), &(led2.flash.shift), 180);
+void choose_shift_flash(menu_ *menu){
+	switch(menu->pos){
+		case 3:
+			led1.change = true;
+			led2.change = false;
+			choose_time(&(led1.flash.shift), &(led2.flash.shift), 180);
+			break;
+		case 4:
+			led1.change = false;
+			led2.change = true;
+			choose_time(&(led1.flash.shift), &(led2.flash.shift), 230);
+			break;
+	}
 }
 
 void create_flash_menu(menu_ *menu){
-	menu->buttons_number = 4;
+	menu->buttons_number = 5;
 	menu->pos = 0;
 	menu->button0 = "Led 1";
 	menu->button1 = "Led 2";
 	menu->button2 = "Both";
-	menu->button3 = "Shift";
+	menu->button3 = "Shift 1 to 2";
+	menu->button4 = "Shift 2 to 1";
 	menu->comment = "exit";
 	menu->comment2 = "choose";
 	
@@ -78,6 +88,7 @@ void create_flash_menu(menu_ *menu){
 	menu->func1 = &go_next_menu;
 	menu->func2 = &go_next_menu;
 	menu->func3 = &choose_shift_flash;
+	menu->func4 = &choose_shift_flash;
 	set_no_links(menu);
 }
 
